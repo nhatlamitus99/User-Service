907926f385d5c05e470dcc403da230858657755d
Phong Vo <phongvxk38@gmail.com>
2019-10-21 23:31:22 +0700

    First commit

---
+++ models/userModel.go
@@ -0,0 +1,55 @@
+package models
+
+import (
+	"GolangRestApi/entities"
+	"errors"
+)
+
+var (
+	listUser = make([]*entities.User, 0)
+)
+
+func CreateUser(user *entities.User) bool {
+	if user.Id != "" && user.Name != "" && user.Password != "" {
+		if userF, _ := FindUser(user.Id); userF == nil {
+			listUser = append(listUser, user)
+			return true
+		}
+	}
+	return false
+}
+
+func UpdateUser(eUser *entities.User) bool {
+	for index, user := range listUser {
+		if user.Id == eUser.Id {
+			listUser[index] = eUser
+			return true
+		}
+	}
+	return false
+}
+
+func FindUser(id string) (*entities.User, error) {
+	for _, user := range listUser {
+		if user.Id == id {
+			return user, nil
+		}
+	}
+	return nil, errors.New("User doesn't exist")
+}
+
+func DeleteUser(id string) bool {
+	for index, user := range listUser {
+		if user.Id == id {
+			copy(listUser[index:], listUser[index+1:])
+			listUser[len(listUser)-1] = &entities.User{}
+			listUser = listUser[:len(listUser)-1]
+			return true
+		}
+	}
+	return false
+}
+
+func GetAllUser() []*entities.User {
+	return listUser
+}
