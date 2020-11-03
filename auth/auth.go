c2746a1b6d7ee192cfa8038f8149817cccd0da17
nhatlamitus99 <ngophannhatlam@gmail.com>
2020-11-03 17:46:37 +0700

    refactor code

--- auth/auth.go
+++ auth/auth.go
@@ -1,12 +1,9 @@
 package auth
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -21,14 +18,12 @@ func CreateToken(username, password string) (string, error) {
 	claims["username"] = username
 	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	fmt.Println("key", os.Getenv("SECRET_KEY"))
 	return token.SignedString([]byte(os.Getenv("SECRET_KEY")))
-
 }
 
-func TokenValid(r *http.Request) (error, entities.Data) {
+func TokenValid(request *http.Request) (error, entities.Data) {
 	data := entities.Data{}
-	tokenString := ExtractToken(r)
+	tokenString := ExtractToken(request)
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
@@ -42,54 +37,15 @@ func TokenValid(r *http.Request) (error, entities.Data) {
 		fmt.Println(claims)
 		data.Username = claims["username"].(string)
 		data.Password = claims["password"].(string)
-		Pretty(claims)
 	}
 	return nil, data
 }
 
-func ExtractToken(r *http.Request) string {
-	keys := r.URL.Query()
-	token := keys.Get("token")
-	if token != "" {
-		return token
-	}
-	bearerToken := r.Header.Get("Authorization")
+func ExtractToken(request *http.Request) string {
+
+	bearerToken := request.Header.Get("Authorization")
 	if len(strings.Split(bearerToken, " ")) == 2 {
 		return strings.Split(bearerToken, " ")[1]
 	}
 	return ""
-}
-
-func ExtractTokenID(r *http.Request) (uint32, error) {
-
-	tokenString := ExtractToken(r)
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(os.Getenv("API_SECRET")), nil
-	})
-	if err != nil {
-		return 0, err
-	}
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if ok && token.Valid {
-		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
-		if err != nil {
-			return 0, err
-		}
-		return uint32(uid), nil
-	}
-	return 0, nil
-}
-
-func Pretty(data interface{}) string {
-	b, err := json.MarshalIndent(data, "", " ")
-	if err != nil {
-		log.Println(err)
-		return err.Error()
-	}
-
-	fmt.Println(string(b))
-	return string(b)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/gorilla/mux v1.7.3
 	github.com/lib/pq v1.8.0
 	gorm.io/driver/postgres v1.0.5
 	gorm.io/gorm v1.20.5
